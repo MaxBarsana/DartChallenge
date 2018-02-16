fe8b85ba9575d47cbb0e4f85d049d2e7d4e23d98
Max Barsana <maxbarsana@gmail.com>
2018-02-16 12:03:04 -0800

    Dart Game Simulation

---
+++ ChallengeSimpleDarts/DartsChallenge/CallengeSimpleDarts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CallengeSimpleDarts
+{
+    public class Player
+    {
+
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+
+    }
+}
